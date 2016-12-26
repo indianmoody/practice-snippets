5804d76976f61e755201e184425029f2e8830238
indianmoody <gaurav.bishnoi@colorado.edu>
2016-12-26 15:21:17 -0700

    next largest

--- NextNumber.java
+++ NextNumber.java
@@ -31,6 +31,34 @@ class NextNumber {
     
   }
   
+  // next largest number with same no of 1s
+  public static int nextLargest(int n) {
+    if (n == 0) {
+      return 0;
+    }
+    
+    int ans = n;
+    int x0 = 0, x1 = 0;
+    
+    while (n%2 == 1) {
+      x0++;
+      n = n/2;
+    }
+    
+    while (n%2 == 0) {
+      x0++;
+      n = n/2;
+    }
+    
+    x1 = x0;
+    x0 = x0 - 1;
+    
+    ans = ans | (1<<x0);
+    ans = ans & (~(1<<x1));
+    
+    return ans;
+  }
+  
   public static void main(String[] args) {
     int n = 27;
     System.out.printf("number n: %d, binary: %s\n", n,  Integer.toBinaryString(n));
